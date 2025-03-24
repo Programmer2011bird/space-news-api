73c82ca740dd9dc726e2a526d6b450fe44ab5735
Programmer2011bird <maysamshaker7@gmail.com>
2025-03-24 04:22:46 +0330

    formatting the data to dict

---
+++ scraper/formatter.py
@@ -0,0 +1,43 @@
+from scraper import scraper
+import datetime
+
+
+class formatter:
+    def __init__(self) -> None:
+        self.scraper: scraper = scraper()
+        self.NEWS_DIVS: list = self.scraper.scrape()
+    
+    def format(self):
+        self.WHOLE_INFO: list[dict[str, str | datetime.date]] = []
+
+        for index in range(len(self.NEWS_DIVS) - 1):
+            try:
+                CONTENT = self.NEWS_DIVS[index + 1].find('div', attrs={'class':'content'})
+                print(CONTENT)
+    
+                CATEGORY: str = self.NEWS_DIVS[index + 1].find('a', attrs={'class':'category-link'}).text
+                print(CATEGORY)
+    
+                NAME: str = CONTENT.find('h3', attrs={'class':'article-name'}).text
+                SUMMARY: str = CONTENT.find('p', attrs={'class': 'synopsis'}).text
+                DATE: datetime.date = datetime.date.today()
+    
+                ITERATION_INFO: dict[str, str | datetime.date] = {
+                    'Name': NAME,
+                    'Category': CATEGORY,
+                    'Date': DATE,
+                    'Summary': SUMMARY
+                }
+    
+                self.WHOLE_INFO.append(ITERATION_INFO)
+
+            
+            except AttributeError:
+                pass
+
+        return self.WHOLE_INFO
+
+
+if __name__ == "__main__":
+    FORMATTER: formatter = formatter()
+    print(FORMATTER.format())
