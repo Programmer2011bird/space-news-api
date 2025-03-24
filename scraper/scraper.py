7a65dc7998eadb64b291c334d115407c867528d9
Programmer2011bird <maysamshaker7@gmail.com>
2025-03-24 04:22:44 +0330

    removed unneccessary code

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,25 +1,27 @@
 from bs4 import BeautifulSoup
-from lxml import etree
+import datetime
 import requests
 
 
 class scraper:
     def __init__(self) -> None:
         self.URL: str = "https://www.space.com/news"
+    
+    def scrape(self) -> list:
         self.RESPONSE: requests.Response = requests.get(self.URL)
         self.HTML_CONTENT: str = self.RESPONSE.text
 
         self.SOUP: BeautifulSoup = BeautifulSoup(self.HTML_CONTENT, 'html.parser')
+        print(type(self.SOUP))
         self.NEWS_DIVS: list = []
+        self.CONTENT_DIVS: list = []
 
         for i in range(11):
             self.NEWS_DIVS.append(self.SOUP.find('div', attrs={'class':f'listingResult small result{i}'}))
         
-        for DIV in self.NEWS_DIVS[1:]:
-            print(DIV.find_all('div', attrs={'class':'content'}))
-        
+        return self.NEWS_DIVS
+    
 
 if __name__ == "__main__":
     SCRAPER: scraper = scraper()
-# /html/body/div[6]/div[4]/article/div/div[1]/div[5]/div/section/div/div[4]
-# /html/body/div[6]/div[4]/article/div/div[1]/div[5]/div/section/div/div[9]
+    SCRAPER.scrape()
