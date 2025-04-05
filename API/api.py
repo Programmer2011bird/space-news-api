300eb8c165330379500c4be47c9dcf54ff66ea66
Programmer2011bird <maysamshaker7@gmail.com>
2025-04-05 22:40:48 +0330

    added support for search with date + changed endpoints around a little bit

--- API/api.py
+++ API/api.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append(str(Path(__file__).parent.parent))
 
 import database.cursor as cursor
+import datetime
 import fastapi
 
 
@@ -11,14 +12,20 @@ API: fastapi.FastAPI = fastapi.FastAPI()
 DB_CONTROLLER: cursor.DB_CONTROLLER = cursor.DB_CONTROLLER()
 
 
-@API.get("/search/{query}")
+@API.get("/search/keyword/{query}")
 async def search_keyword(query: str):
     result: list = DB_CONTROLLER.search_title(query)
 
     return {"results": result}
 
-@API.get("/category/{query}")
+@API.get("/search/category/{query}")
 async def search_category(query: str):
     result: list = DB_CONTROLLER.search_category(query)
+
+    return {"results": result}
+
+@API.get("/search/date/{year}-{month}-{day}")
+async def search_date(year: str, month: str, day: str):
+    result: list = DB_CONTROLLER.search_date(datetime.datetime(int(year), int(month), int(day)))
 
     return {"results": result}
