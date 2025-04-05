460d6d1ae97c9b1e7172f4ba8c83c90e80e54fc4
Programmer2011bird <maysamshaker7@gmail.com>
2025-04-05 22:18:13 +0330

    added search for category + adjustments for fastapi

--- API/api.py
+++ API/api.py
@@ -1,14 +1,24 @@
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).parent.parent))
+
 import database.cursor as cursor
 import fastapi
 
 
 API: fastapi.FastAPI = fastapi.FastAPI()
+DB_CONTROLLER: cursor.DB_CONTROLLER = cursor.DB_CONTROLLER()
 
 
 @API.get("/search/{query}")
 async def search_keyword(query: str):
-    DB_CONTROLLER: cursor.DB_CONTROLLER = cursor.DB_CONTROLLER()
     result: list = DB_CONTROLLER.search_title(query)
 
     return {"results": result}
 
+@API.get("/category/{query}")
+async def search_category(query: str):
+    result: list = DB_CONTROLLER.search_category(query)
+
+    return {"results": result}
