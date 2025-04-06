48fd97ac6e671bcb68c806dc40cad6729a550444
Programmer2011bird <maysamshaker7@gmail.com>
2025-04-06 21:54:36 +0330

    added endpoints for searching for news between dates

--- API/api.py
+++ API/api.py
@@ -29,3 +29,13 @@ async def search_date(year: str, month: str, day: str):
     result: list = DB_CONTROLLER.search_date(datetime.datetime(int(year), int(month), int(day)))
 
     return {"results": result}
+
+@API.get("/search/date/start_date={start_year}-{start_month}-{start_day}&end_date={end_year}-{end_month}-{end_day}")
+async def search_between_dates(start_year: str, start_month: str, start_day: str, end_year: str, end_month: str, end_day: str):
+    print(start_year)
+    start_date: datetime.datetime = datetime.datetime(int(start_year), int(start_month), int(start_day))
+    end_date: datetime.datetime = datetime.datetime(int(end_year), int(end_month), int(end_day))
+
+    result: list = DB_CONTROLLER.search_between_dates(start_date, end_date)
+
+    return {"results": result}
