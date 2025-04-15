9068c797631c9a80b4867e76b0b4d416a4eed42c
Programmer2011bird <maysamshaker7@gmail.com>
2025-04-16 00:01:10 +0330

    added headers

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,10 +4,14 @@ import requests
 
 class scraper:
     def __init__(self) -> None:
+        self.HEADERS: dict[str, str] = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0",
+            "Accept-Language": "en-US,en;q=0.5"
+        }
         self.URL: str = "https://www.space.com/news"
     
     def scrape(self) -> list:
-        self.RESPONSE: requests.Response = requests.get(self.URL)
+        self.RESPONSE: requests.Response = requests.get(self.URL, headers=self.HEADERS)
         self.HTML_CONTENT: str = self.RESPONSE.text
 
         self.SOUP: BeautifulSoup = BeautifulSoup(self.HTML_CONTENT, 'html.parser')
@@ -20,7 +24,7 @@ class scraper:
         return self.NEWS_DIVS
     
     def scrape_whole_article(self, link: str) -> str:
-        self.RESPONSE: requests.Response = requests.get(link)
+        self.RESPONSE: requests.Response = requests.get(link, headers=self.HEADERS)
         self.SOUP: BeautifulSoup = BeautifulSoup(self.RESPONSE.text, "html.parser")
         self.PARENT_DIV = self.SOUP.find("div", attrs={"id":"article-body"})
         self.PARAGRAPHS: list = self.PARENT_DIV.find_all("p")
